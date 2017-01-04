c504f666f1a9f9e638e57a5dec5d3d54cce04ce9
Leo <foever3580ya@gmail.com>
2017-01-05 00:25:24 +0800

    update TODO.txt && thinking in Two Sum

--- Algorithm/1_Two_Sum.c
+++ Algorithm/1_Two_Sum.c
@@ -19,3 +19,26 @@ int* twoSum(int* nums, int numsSize, int target)
 }
 
 //time: O(n^2)
+
+/* Draft */
+int* twoSum(int* nums, int numsSize, int target)
+{
+	find the smallest number "bias"
+
+	if smallest number < 0
+	all the number in nums add the bias
+	to make sure all the number > 0
+	(maybe this is no need)
+
+	use a array with size (bias + target)
+	initiate it with all -1
+
+	for each number nums[i]
+		find the diverse btw target and nums
+		if (array[diverse] != -1)
+			the answer is nums index and array[diverse]
+		else 
+			array[diverse] = i (i is the index of the current number)
+}
+
+//time: O(n)
