fa8e663d3671b2e6fda149516b4183f3fcd7d485
Leo <forever3580ya@gmail.com>
2017-01-04 23:04:16 +0800

    update two sum

---
+++ Algorithm/1_Two_Sum.c
@@ -0,0 +1,21 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int* twoSum(int* nums, int numsSize, int target)
+{
+	int i, j;
+	int* ans = malloc(sizeof(int) * 2);
+	for (i = 0; i < numsSize; i++) {
+		for (j = i + 1; j < numsSize; j++) {
+			if ((nums[i] + nums[j]) == target) {
+				ans[0] = i;
+				ans[1] = j;
+				return ans;
+			}
+		}
+	}
+	return ans;
+}
+
+//time: O(n^2)
